25a7d88f8a91f86e5f24e51ef16c52cc5d8b436d
des-tinyDM <destinyleaross@gmail.com>
2018-10-29 20:13:18 -0500

    simple register component and logic added

---
+++ client/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="Email" v-model='email'/>
+    <input type="password" name="password" placeholder="Password" v-model='password'/>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({ email: this.email, password: this.password })
+      console.log(response.data)
+    }
+  }
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed', value)
+  //   }
+  // },
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.email = 'hello world'
+  //   }, 200)
+  // }
+}
+</script>
